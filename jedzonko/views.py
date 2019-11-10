80d40bce6407713cabff6fdb737c3dd9bd025120
Krzysztof-Nerwinski <k.nerwinski@gmail.com>
2019-11-10 17:16:41 +0100

    index.html linked to main page and to / url

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -7,5 +7,4 @@ from django.views import View
 class IndexView(View):
 
     def get(self, request):
-        ctx = {"actual_date": datetime.now()}
-        return render(request, "test.html", ctx)
+        return render(request, "index.html")
