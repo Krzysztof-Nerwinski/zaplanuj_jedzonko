e3bbb75588dcd5b79051ab4723b50e5ff6299ae0
Gosia5 <malgorzata@gmail.com>
2019-11-19 12:27:21 +0100

    slug_contact: slug_contact

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -25,7 +25,7 @@ class IndexView(View):
         random.shuffle(list)
         for i in range(3):
             carusel.append((Recipe.objects.get(pk=list[i]).name, Recipe.objects.get(pk=list[i]).description))
-        return render(request, "index.html", context={'carusel': carusel, 'slug_about': slug_about})
+        return render(request, "index.html", context={'carusel': carusel, 'slug_about': slug_about, 'slug_contact': slug_contact})
 
 
 class DashboardView(View):
